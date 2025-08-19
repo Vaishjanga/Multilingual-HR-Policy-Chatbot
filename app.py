446f8a22fe2b566dfe8c23ed9402de9833fa3b18
Janga Thirumala Sai Mahi Vaishnavi <167735768+Vaishjanga@users.noreply.github.com>
2025-08-19 21:30:19 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,331 @@
+import streamlit as st  
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from langchain_community.retrievers import BM25Retriever
+from langchain.retrievers import EnsembleRetriever, ContextualCompressionRetriever
+from langchain.retrievers.document_compressors import DocumentCompressorPipeline
+from langchain_cohere import CohereRerank
+from langchain.prompts import ChatPromptTemplate
+from langchain.retrievers.document_compressors import LLMChainExtractor
+from langchain_chroma import Chroma
+from langchain_community.embeddings.fastembed import FastEmbedEmbeddings
+from streamlit_mic_recorder import mic_recorder
+from bhashini_translator import Bhashini #custom module
+import base64
+from populate_database import load_chunks
+from langchain_core.documents import BaseDocumentTransformer, Document
+from langchain_core.pydantic_v1 import BaseModel, Field
+
+from typing import Any, Callable, List, Sequence
+
+# Stored at local paths.
+CHROMA_PATH = "chroma" 
+DATA_PATH = "data"
+
+# Language set by the user.
+sourceLanguage = "hi"
+targetLanguage = "en"
+
+# Streamlit messages UI templates.
+css = '''
+<style>
+.chat-message {
+    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
+}
+.chat-message.user {
+    background-color: #2b313e
+}
+.chat-message.bot {
+    background-color: #475063
+}
+.chat-message .avatar {
+  width: 20%;
+}
+.chat-message .avatar img {
+  max-width: 78px;
+  max-height: 78px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+.chat-message .message {
+  width: 80%;
+  padding: 0 1.5rem;
+  color: #fff;
+}
+'''
+
+bot_template = '''
+<div class="chat-message bot">
+    <div class="avatar">
+        <img src="https://i.ibb.co/cN0nmSj/Screenshot-2023-05-28-at-02-37-21.png">
+    </div>
+    <div class="message">{{MSG}}</div>
+</div>
+'''
+
+# replace image with User's profile photo.
+user_template = '''
+<div class="chat-message user">
+    <div class="avatar">
+        <img src="https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?q=80&w=2680&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3Dg">
+    </div>    
+    <div class="message">{{MSG}}</div>
+</div>
+'''
+
+def get_embedding_function():
+    """
+    Get the FastEmbed embedding function for document embeddings.
+
+    Returns:
+        FastEmbedEmbeddings: An instance of FastEmbedEmbeddings for creating document embeddings.
+    """
+    embeddings = FastEmbedEmbeddings()
+    return embeddings
+
+def get_vectorstore():
+    """
+    Load the vector store from ChromaDB.
+
+    Returns:
+        Tuple[Chroma, list[Document]]:
+            - Chroma object: The loaded vector store.
+            - list[Document]: The document chunks used to create the vector store.
+    """
+    embedding_function = get_embedding_function()
+    chunks = load_chunks()
+    if chunks is None:
+        st.error("No saved chunks found. Please run populate_database.py first.")
+        return None, None
+    db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
+    return db, chunks
+
+class RelevanceScoreFilter(BaseDocumentTransformer, BaseModel):
+    """Filter that drops documents below a certain relevance score threshold."""
+    
+    relevance_threshold: float = Field(default=0.5, ge=0.0, le=1.0)
+    """Threshold for determining when a document is relevant enough to be included."""
+
+    class Config:
+        arbitrary_types_allowed = True
+
+    def transform_documents(
+        self, documents: Sequence[Document], **kwargs: Any
+    ) -> List[Document]:
+        """Filter down documents based on relevance scores."""
+        filtered_documents = []
+        for doc in documents:
+            if 'relevance_score' in doc.metadata and doc.metadata['relevance_score'] >= self.relevance_threshold:
+                filtered_documents.append(doc)
+        return filtered_documents
+
+    def _call(
+        self,
+        documents: Sequence[Document],
+        **kwargs: Any,
+    ) -> List[Document]:
+        return self.transform_documents(documents, **kwargs)
+
+def get_improved_retriever(vectorstore, chunks):
+    """
+    Get an advanced retriever with hybrid search.
+
+    Args:
+        vectorstore (Chroma): used for semantic search.
+        chunks (list[Document]): used for keyword search.
+
+    Returns:
+        ContextualCompressionRetriever: Improved retriever combining vector and keyword search, as well as a reranker.
+    """
+    # Vector store retriever
+    vectorstore_retriever = vectorstore.as_retriever(search_kwargs={"k": 8})
+    
+    # Keyword retriever
+    bm25_retriever = BM25Retriever.from_documents(chunks)
+    bm25_retriever.k = 8
+    
+    # Ensemble retriever
+    ensemble_retriever = EnsembleRetriever(
+        retrievers=[vectorstore_retriever, bm25_retriever],
+        weights=[0.5, 0.5]
+    )
+    cohere_compressor = CohereRerank(model="rerank-english-v3.0", top_n=5)
+    relevance_filter = RelevanceScoreFilter(relevance_threshold=0.76)
+    llm = ChatOpenAI(temperature=0, model="gpt-4o-mini")
+    compressor = LLMChainExtractor.from_llm(llm)
+
+    pipeline_compressor = DocumentCompressorPipeline(
+        transformers=[cohere_compressor, relevance_filter, compressor]
+    )
+    
+    compression_retriever = ContextualCompressionRetriever(
+        base_compressor=pipeline_compressor, base_retriever=ensemble_retriever
+    )
+    return compression_retriever
+
+def get_conversation_chain(retriever):
+    """
+    Get a conversational chain using the provided retriever.
+
+    Args:
+        retriever (ContextualCompressionRetriever): The retriever to use in the chain.
+
+    Returns:
+        ConversationalRetrievalChain: A chain that combines the language model, retriever, and conversation memory.
+    """
+    system_prompt = """You are a helpful assistant for the Government of India's National Career Service. 
+    Provide accurate, concise information about career centers, job opportunities, and related services. 
+    Use simple language and give specific details when available. If unsure, say so without making up information."""
+    
+    human_prompt = """Context: {context}
+    
+    Human: {question}
+    
+    Assistant: Let's approach this step-by-step:
+    1) First, I'll determine the specific information need to answer the question, including info such as the state / location that the query asks for or the type of training center.
+    2) Then, I'll review the relevant information from the context by searching through all of the documents.
+    3) After that, I'll provide a clear and concise answer to your question without making up things.
+    4) If any details are missing or unclear, I'll mention that.
+    
+    Here's my response:
+    """
+    
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", system_prompt),
+        ("human", human_prompt),
+    ])
+    
+    llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+    
+    chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=retriever,
+        memory=ConversationBufferMemory(memory_key='chat_history', return_messages=True, output_key='answer'),
+        combine_docs_chain_kwargs={"prompt": prompt},
+        return_source_documents=True,
+        return_generated_question=True 
+    )
+    
+    return chain
+
+def handle_userinput(user_question):
+    """
+    Process user input, generate a response, update the chat history, and display results on the Streamlit application.
+
+    This function retrieves Chatbot responses, translates messages, and generates text-to-speech output for the bot's responses.
+
+    Args:
+        user_question (str): The user's input question translated to English.
+    """
+    bhashini = Bhashini("en", sourceLanguage)
+    processed_question = f"Be revelant. If the context provided isn't neccessary, don't add it to your response. However, make your response accurate and complete by only using information from the provided context. Use addresses. Use bullet points for lengthy responses.  User: {user_question}"
+
+    response = st.session_state.conversation({'question': processed_question})
+    st.session_state.chat_history = response['chat_history']
+    chat_history = response['chat_history']
+
+    print("Retrieved Documents:")
+    for i, doc in enumerate(response['source_documents']):
+        print(f"Document {i+1}:")
+        print(f"Content: {doc.page_content}")
+        print(f"Metadata: {doc.metadata}")
+        print("---------")
+
+    print(f"Generated Question: {response['generated_question']}")
+    translated_new_messages = []
+
+    for index, message in enumerate(chat_history):
+        message_id = str(index)
+        if message_id not in st.session_state.translated_messages_record:
+            st.session_state.translated_messages_record.add(message_id)
+            if hasattr(message, 'content'):
+                if index % 2 == 0:
+                    st.write("Receiving message with original prompt, will cut for translation: ", message)
+                    user_question_content = getattr(message, 'content').split("User: ")[1]
+                    translated_message_content = bhashini.translate(user_question_content)
+                    st.write("User message, no need for audio: ", user_question_content)
+                    base64_aud = ""
+                else:
+                    bot_question_content = getattr(message, 'content')
+                    translated_message_content = bhashini.translate(bot_question_content)
+                    st.write("Bot response, generating audio: ", bot_question_content)
+                    bhashini2 = Bhashini(sourceLanguage, targetLanguage)
+                    base64_aud = bhashini2.tts(translated_message_content)
+
+                translated_new_messages.append({
+                    'text': translated_message_content,
+                    'audio': base64_aud
+                })
+
+    st.session_state.translated_chat_history.extend(translated_new_messages)
+    for i, message_data in enumerate(st.session_state.translated_chat_history):
+        translated_message = message_data['text']
+        base64_aud = message_data['audio']
+
+        if i % 2 == 0:
+            st.write(user_template.replace("{{MSG}}", translated_message), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace("{{MSG}}", translated_message), unsafe_allow_html=True)
+            st.audio(base64.b64decode(base64_aud), format="audio/wav")
+            
+def main():
+    """
+    Main function that runs the Streamlit application.
+
+    This function initializes the Streamlit interface, loads the database, and handles user queries and interactions.
+    """
+    load_dotenv()
+    st.set_page_config(page_title="ChauwkBot", page_icon=":books:")
+    st.write(css, unsafe_allow_html=True)
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+    if "translated_chat_history" not in st.session_state:
+        st.session_state.translated_chat_history = []
+    if "translated_messages_record" not in st.session_state:
+        st.session_state.translated_messages_record = set()
+
+    st.header("Chauwk Bot")
+    user_question = st.text_input("Ask away!")
+
+    voice_recording_column, send_button_column = st.columns(2)
+    with voice_recording_column:
+        voice_recording = mic_recorder(start_prompt="Start recording", stop_prompt="Stop recording", format="wav")
+    with send_button_column:
+        send_button = st.button("Send", key="send_button")
+
+    if send_button:
+        if user_question:
+            bhashini = Bhashini(sourceLanguage, "en")
+            user_question = bhashini.translate(user_question)
+            handle_userinput(user_question)
+            user_question = None
+        elif voice_recording:
+            audio_base64_string = base64.b64encode(voice_recording['bytes']).decode('utf-8')
+            # save audio_base64_string to a text file
+            with open("audio.txt", "w") as text_file:
+                text_file.write(audio_base64_string)
+            bhashini = Bhashini(sourceLanguage, targetLanguage)
+            text = bhashini.asr_nmt(audio_base64_string)
+            user_question = text
+            handle_userinput(user_question)
+            voice_recording = None
+
+    if not user_question:
+        user_question = None
+        voice_recording = None
+
+    with st.sidebar:
+        if st.button("Load Database"):
+            with st.spinner("Loading"):
+                vectorstore, chunks = get_vectorstore()
+                retriever = get_improved_retriever(vectorstore, chunks)
+                st.session_state.conversation = get_conversation_chain(retriever)
+                st.write("✅ Loaded the database")
+
+if __name__ == '__main__':
+    main()
---
+++ populate_database.py
@@ -0,0 +1,346 @@
+import argparse
+import os
+import glob
+import shutil
+import json
+import pytesseract
+from langchain_community.document_loaders import DirectoryLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.schema.document import Document
+from langchain_chroma import Chroma
+from dotenv import load_dotenv
+from llama_parse import LlamaParse
+#from llama_index.core import SimpleDirectoryReader
+#from llama_index.readers.file import SimpleDirectoryReader
+
+from langchain_community.document_loaders import UnstructuredFileLoader
+import pickle
+from langchain_community.embeddings.fastembed import FastEmbedEmbeddings
+from img2table.ocr import TesseractOCR
+from img2table.document import PDF
+
+# Stored at local paths.
+#pytesseract.pytesseract.tesseract_cmd = r"/usr/bin/tesseract"
+
+
+
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract"
+
+CHROMA_PATH = "chroma"
+DATA_PATH = "./data/"
+PARSING_DATA_PATH = "./parsing_data/"
+CHUNKS_PATH = "processed_chunks.pkl"
+PARSED_FILES_LIST = "parsed_files.json"
+
+# LlamaParse API key from .env file.
+llamaparse_api_key = os.getenv("LLAMA_CLOUD_API_KEY")
+
+def main():
+    """
+    Main function that runs the Chroma database population process.
+
+    To add files with text paragraphs, change the extract_tables_from_pdf() call with load_documents() and run 'python populate_database.py' without the reset flag.
+
+    Run the populate_database.py script with the '--reset" flag to fully clear the database before adding files in the data path.
+    """
+    load_dotenv()
+    # Check if the database should be cleared (using the --reset flag).
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--reset", action="store_true", help="Reset the database.")
+    args = parser.parse_args()
+
+    os.makedirs('llama_parsed', exist_ok=True)
+    os.makedirs(PARSING_DATA_PATH, exist_ok=True)
+
+    if args.reset:
+        print("✨ Clearing Database")
+        clear_database()
+
+    # Update data store.
+    documents = extract_tables_from_pdf(DATA_PATH) # switch this with the load_documents() function for PDF files with text paragraphs.
+    chunks = split_documents(documents)
+    save_chunks(chunks)
+    add_to_chroma(chunks)
+
+def extract_tables_from_pdf(directory_path):
+    """
+    Extract ONLY tables from a PDF document using img2table and return structured data - use for PDFs with only tables.
+
+    If the PDF contains text paragraphs, this function will not extract it. Instead, use the load_documents() functionLlamaParse.
+    
+    Returns:
+        List[dict]: A dictionary of extracted tables keyed by file name.
+    """
+    parsed_files = load_parsed_files()
+    files_to_parse = []
+
+    for file in os.listdir(DATA_PATH):
+        if file not in parsed_files:
+            files_to_parse.append(file)
+            shutil.copy(os.path.join(DATA_PATH, file), PARSING_DATA_PATH)
+
+    if not files_to_parse:
+        print("No new files to parse.")
+        return load_parsed_documents()
+    if (len(files_to_parse) == 1):
+        print(f"Parsing {len(files_to_parse)} new file...")
+    else:
+        print(f"Parsing {len(files_to_parse)} new files...")
+    ocr = TesseractOCR(n_threads=1, lang="eng")
+    all_extracted_tables = []
+    for filename in os.listdir(directory_path):
+        if filename.endswith(".pdf"):
+            pdf_path = os.path.join(directory_path, filename)
+            doc = PDF(pdf_path)
+            extracted_tables = doc.extract_tables(ocr=ocr, implicit_rows=False, borderless_tables=False, min_confidence=50)
+            
+            all_extracted_tables.append({filename: extracted_tables})
+            
+            with open('llama_parsed/output.md', 'a') as f:
+                for key, value_list in extracted_tables.items():
+                    for value in value_list:
+                        table_df = value.df
+                        f.write(f"Table {key} from {filename}:\n")
+                        f.write(table_df.to_string())
+                        f.write("\n\n")
+    
+    parsed_files.extend(files_to_parse)
+    save_parsed_files(parsed_files)
+
+    for file in os.listdir(PARSING_DATA_PATH):
+        os.remove(os.path.join(PARSING_DATA_PATH, file))
+    return load_parsed_documents()
+
+def load_parsed_documents():
+    """
+    Load all parsed documents from the 'output.md' file.
+
+    Returns:
+        List[Documents]: A list containing the parsed content.
+    """
+    loader = UnstructuredFileLoader('llama_parsed/output.md')
+    documents = loader.load()
+    print("Loaded parsed documents")
+    return documents
+
+def split_documents(documents: list[Document]):
+    """
+    Split the input documents into smaller chunks for processing.
+
+    Args:
+        documents (List[Document]): A list of documents to be split.
+
+    Returns:
+        List[Document]: A list containing the split chunks.
+    """
+    text_splitter = RecursiveCharacterTextSplitter(
+        separators=["\n\n"]
+    )
+    return text_splitter.split_documents(documents)
+
+def save_chunks(chunks: list[Document]):
+    """
+    Save processed document chunks to a pickle file.
+
+    Args:
+        chunks (List[Document]): A list containing the processed chunks.
+    """
+    with open(CHUNKS_PATH, 'wb') as f:
+            pickle.dump(chunks, f)
+    print(f"✅ Saved {len(chunks)} chunks to {CHUNKS_PATH}")
+
+def load_chunks():
+    """
+    Load previously saved document chunks from the pickle file.
+
+    Returns:
+        None: If no saved chunks are found
+
+        or
+
+        List[Document]: A list containing the processed chunks.
+    """
+    if os.path.exists(CHUNKS_PATH):
+        with open(CHUNKS_PATH, 'rb') as f:
+            chunks = pickle.load(f)
+        print(f"✅ Loaded {len(chunks)} chunks from {CHUNKS_PATH}")
+        return chunks
+    else:
+        print("❌ No saved chunks found.")
+        return None
+
+def load_parsed_files():
+    """
+    Load the list of previously parsed files.
+
+    Returns:
+        List[str]: A list of filenames that have already been parsed.
+    """
+    if os.path.exists(PARSED_FILES_LIST):
+        with open(PARSED_FILES_LIST, 'r') as f:
+            return json.load(f)
+    return []
+
+def load_documents(directory_path):
+    """
+    Load and parse new documents from the data directory using LlamaParse - use for files with text / paragraph.
+
+    This function uses LlamaParse to process PDF files into a structured format.
+
+    Returns:
+        List[Documents]: A list containing the parsed content.
+    """
+    parsed_files = load_parsed_files()
+    files_to_parse = []
+
+    for file in os.listdir(DATA_PATH):
+        if file not in parsed_files:
+            files_to_parse.append(file)
+            shutil.copy(os.path.join(DATA_PATH, file), PARSING_DATA_PATH)
+
+    if not files_to_parse:
+        print("No new files to parse.")
+        return load_parsed_documents()
+    if (len(files_to_parse) == 1):
+        print(f"Parsing {len(files_to_parse)} new file...")
+    else:
+        print(f"Parsing {len(files_to_parse)} new files...")
+    parser = LlamaParse(
+        api_key=llamaparse_api_key,
+        result_type="markdown",
+        parsing_instruction="""
+            Using the format Column Name : Field Value, convert all of the fields with headers of one row into one single line of text. 
+            Example:
+                S.No: 1; State / UT: Assam; Location: Guwahati, Employment Exchange; Address: District Employment Exchange, Guwahati AK Azad Road, Rehabari, Guwahati-8; Type Of Center: Employment Exchange.
+                S.No: 10; JSS NAME: JSS Ongole; STATE: Andhra Pradesh; DISTRICT: Prakasham; ADDRESS: H.No.3-119/1, Satyanarayanapuram, 2nd Lane, Ongole, MARKAPUR-523002, PRAKASAM (Andhra Pradesh); EMAIL: jss.ongole@gmail.com; MOBILE: 8333046955; COURSES THEY ARE OFFERING: Handicrafts & Carpets; PIN CODE: 523002; Type of Training Centre: JSS.
+            Use semicolons to separate different fields.
+            """,
+        skip_diagonal_text=True
+    )
+    print("loaded parser")
+    file_extractor = {".pdf": parser}
+    llama_documents = SimpleDirectoryReader(input_dir=PARSING_DATA_PATH, file_extractor=file_extractor).load_data()
+    print("created llama_parse documents")
+    with open('llama_parsed/output.md', 'a') as f: 
+        for doc in llama_documents:
+            f.write(doc.text + '\n')
+    
+    parsed_files.extend(files_to_parse)
+    save_parsed_files(parsed_files)
+
+    for file in os.listdir(PARSING_DATA_PATH):
+        os.remove(os.path.join(PARSING_DATA_PATH, file))
+
+    return load_parsed_documents()
+
+def save_parsed_files(parsed_files):
+    """
+    Save the list of parsed files.
+
+    Args:
+        parsed_files (List[str]): A list of filenames that have been parsed.
+    """
+    with open(PARSED_FILES_LIST, 'w') as f:
+        json.dump(parsed_files, f)
+
+def add_to_chroma(chunks: list[Document]):
+    """
+    Add new document chunks to the Chroma vector store.
+
+    This function only adds new documents that don't already exist in the database using chunk IDs.
+
+    Args:
+        chunks (List[Document]): A list of document chunks to be added to the database.
+    """
+    try:
+        db = Chroma(
+            persist_directory=CHROMA_PATH, embedding_function=get_embedding_function()
+        )
+        chunks_with_ids = calculate_chunk_ids(chunks)
+
+        existing_items = db.get(include=[])
+        existing_ids = set(existing_items["ids"])
+        print(f"Number of existing documents in DB: {len(existing_ids)}")
+
+        new_chunks = []
+        for chunk in chunks_with_ids:
+            if chunk.metadata["id"] not in existing_ids:
+                new_chunks.append(chunk)
+
+        print(f"New Chunks to Add: {len(new_chunks)}")
+        if new_chunks:
+            print("Sample New Chunk Metadata:", new_chunks[0].metadata)
+            db.add_documents(new_chunks, ids=[chunk.metadata["id"] for chunk in new_chunks])
+            print("✅ Successfully added new documents to Chroma.")
+        else:
+            print("✅ No new documents to add.")
+    except Exception as e:
+        print(f"Error in `add_to_chroma`: {e}")
+
+
+def calculate_chunk_ids(chunks):
+    """
+    Calculate unique IDs for each document chunk.
+
+    Page Source : Page Number : Chunk Index 
+    "data/Model_Career_Centres.pdf:6:2"
+
+    Args:
+        chunks (List[Document]): A list of document chunks objects to assign IDs to.
+
+    Returns:
+        List[Document]: The input list of document chunk objects with added 'id' metadata.
+    """
+    last_page_id = None
+    current_chunk_index = 0
+
+    for chunk in chunks:
+        source = chunk.metadata.get("source")
+        page = chunk.metadata.get("page")
+        current_page_id = f"{source}:{page}"
+
+        if current_page_id == last_page_id:
+            current_chunk_index += 1
+        else:
+            current_chunk_index = 0
+
+        chunk_id = f"{current_page_id}:{current_chunk_index}"
+        last_page_id = current_page_id
+
+        chunk.metadata["id"] = chunk_id
+
+    return chunks
+
+def get_embedding_function():
+    """
+    Get the FastEmbed embedding function for document embeddings.
+
+    Returns:
+        FastEmbedEmbeddings: An instance of FastEmbedEmbeddings for creating document embeddings.
+    """
+    embeddings = FastEmbedEmbeddings()
+    return embeddings
+
+def clear_database():
+    """
+    Clear the existing database, chunks, and parsed files.
+
+    This function removes all previously processed local input (except for files in the /data directory).
+    """
+    if os.path.exists(CHROMA_PATH):
+        shutil.rmtree(CHROMA_PATH)
+    if os.path.exists(CHUNKS_PATH):
+        os.remove(CHUNKS_PATH)
+    output_path = 'llama_parsed/output.md'
+    if os.path.exists(output_path):
+        with open(output_path, 'w') as f:
+            f.write('')
+    if os.path.exists(PARSED_FILES_LIST):
+        os.remove(PARSED_FILES_LIST)
+    if os.path.exists(PARSING_DATA_PATH):
+        shutil.rmtree(PARSING_DATA_PATH)
+        os.makedirs(PARSING_DATA_PATH)
+    print("✨ Cleared database, chunks, parsed files, and output.md")
+
+if __name__ == "__main__":
+    main()
